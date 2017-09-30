c39dd3fede77ad42291a419f4801709ac22dd838
sweetdream779 <sweetdream779@gmail.com>
2017-09-30 21:14:52 +0300

    Task1

---
+++ Euclid.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Euclid {
+
+	static int method1(int a, int b) {
+		if (b == 0)
+			return a < 0 ? -a : a;
+		return method1(b, a % b);
+	}
+
+	static int method2(int a, int b) {
+		if (b == 0)
+			return a < 0 ? -a : a;
+
+		return method2(b, Math.floorMod(a, b));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		try {
+			System.out.println("Введите целые числа через пробел и нажмите Enter: ");
+			String h = br.readLine();
+			String[] splited = h.split("\\s+");
+
+			if (splited.length != 2) {
+				System.out.println("Вы ввели не 2 числа");
+				System.exit(1);
+			}
+
+			a = Integer.valueOf(splited[0]).intValue();
+			b = Integer.valueOf(splited[1]).intValue();
+
+			System.out.println("НОД числе с помощью %: " + method1(a, b));
+			System.out.println("НОД числе с помощью floorMod: " + method2(a, b));
+			// System.out.println("НОД числе с помощью rem: " + rem(a, b));
+
+		} catch (IOException | NumberFormatException e) {
+			System.out.println("ошибка ввода" + e);
+		}
+	}
+
+}
